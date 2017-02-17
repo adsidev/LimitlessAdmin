59e0363275561cccf9d85471f9bac08d5f94753d
DESKTOP-DSRH9A9\ADSIXPS15 <zcao@goadsi.com>
2017-02-17 17:28:40 -0500

    optimize code, add notification, and exception check

--- Limitless/LimitLess/Area/SpreadsheetController.cs
+++ Limitless/LimitLess/Area/SpreadsheetController.cs
@@ -35,94 +35,40 @@ namespace LimitLess.Area
             _coreModel = new SpreadsheetCoreModel();
         }
 
-        public HttpResponseMessage SaveSpreadsheet()
-        {
+        public bool SaveSpreadsheet()
+        { 
             var httpRequest = HttpContext.Current.Request;
             if (httpRequest.Files.Count < 1)
             {
-                return Request.CreateResponse(HttpStatusCode.BadRequest);
+                return false;
             }
 
             foreach (string file in httpRequest.Files)
             {
                 /*extract excel file and then save the file to App_Data folder*/
                 var postedFile = httpRequest.Files[file];
-                string filePath = HttpContext.Current.Server.MapPath("~/App_Data/" + DateTime.Now.ToString("HHmmss") + postedFile.FileName);
-                postedFile.SaveAs(filePath);
+                string uploadFilePath = HttpContext.Current.Server.MapPath("~/App_Data/uploaded_spreadsheet" + DateTime.Now.ToString("HHmmss") + postedFile.FileName);
+                postedFile.SaveAs(uploadFilePath);
+                string standardFilePath = HttpContext.Current.Server.MapPath("~/Files/standard_files/excel_format.xlsx");
+                /* open the standard excel file and user's excel file */
+                var upload_range = openExcelFile(uploadFilePath);
+                var standard_range = openExcelFile(standardFilePath);
 
-                /*read data from the excel file*/
-                ReadDataFromFile(filePath);
-            }
-
-            return Request.CreateResponse(HttpStatusCode.Created);
-
-        }
-        public void ReadDataFromFile(string filePath)
-        {
-            Excel.Application application = new Excel.Application();
-            Excel.Workbook workbook = application.Workbooks.Open(filePath);
-            Excel.Worksheet worksheet = workbook.ActiveSheet;
-            Excel.Range range = worksheet.UsedRange;
-
-            /*save the data to spreadsheet model*/
-            List<SpreadsheetModel> spreadsheetList = SaveDataToList(range);
-            _coreModel.Save(spreadsheetList);
-        }
-        public string extractID(string str)
-        {
-
-            var regex = new Regex(@"([1-9]+)");
-            var results = regex.Matches(str);
-            var id = results[0].Groups[1].Value;
-            return id;
-        }
-        public List<SpreadsheetModel> SaveDataToList(Excel.Range range)
-        {
-            List<SpreadsheetModel> spreadsheetList = new List<SpreadsheetModel>();
-            for (int row = 2; row <= range.Rows.Count; row++)
-            {
-                //if question content is not none, add this row of data
-                if (((Excel.Range) range.Cells[row, 6]).Text != "")
+                /* check if the uploaded file is in the correct format, save the spreadsheet. Otherwise, refuse to save the data. */
+                if (!checkExcelFormat(standard_range, upload_range))
                 {
-                    var queModel = new QuestionModel();
-                    var ansList = new List<AnswerModel>();
-                    var spr = new SpreadsheetModel
-                    {
-                        questionModel = queModel,
-                        answerList = ansList
-                    };
-                    //add question
-                    spr.questionModel.SubObjectiveID = Int32.Parse(extractID(((Excel.Range)range.Cells[row, 5]).Text), CultureInfo.InvariantCulture);
-                    spr.questionModel.QuestionContent = ((Excel.Range)range.Cells[row, 6]).Text;
-                    spr.questionModel.QuestionTypeId = extractID(((Excel.Range)range.Cells[row, 5]).Text);
-                    spr.questionModel.Difficulty = Int32.Parse(extractID(((Excel.Range)range.Cells[row, 8]).Text), CultureInfo.InvariantCulture);
-                    spr.questionModel.QuestionCode = ((Excel.Range)range.Cells[row, 9]).Text;
-                    spr.questionModel.IsActive = ((Excel.Range)range.Cells[row, 10]).Text;
-                    
-                    //if there is answer content, add the answer
-                    int ans_index = 12;
-                    while (((Excel.Range)range.Cells[row, ans_index]).Text != "")
-                    {
-                        var ans = new AnswerModel();
-                        ans.AnswerContent = ((Excel.Range)range.Cells[row, ans_index]).Text;
-                        ans.Explanation = ((Excel.Range)range.Cells[row, ans_index+1]).Text;
-                        ans.AnswerCode = ((Excel.Range)range.Cells[row, ans_index+2]).Text;
-                        ans.IsActive = ((Excel.Range)range.Cells[row, ans_index+3]).Text;
-                        ans.IsCorrect = ((Excel.Range)range.Cells[row, ans_index+4]).Text; 
-                        spr.answerList.Add(ans);
-                        ans_index += 5;
-                    }
-                    
-                    spreadsheetList.Add(spr);
+                    return false;
                 }
-                
+
+                /*save the data to spreadsheet model*/
+                List<SpreadsheetModel> spreadsheetList = SaveDataToList(upload_range);
+                _coreModel.Save(spreadsheetList);
             }
-            return spreadsheetList;
+            return false;
         }
 
         public HttpResponseMessage SaveImages()
         {
-
             var httpRequest = HttpContext.Current.Request;
             if (httpRequest.Files.Count < 1)
             {
@@ -138,6 +84,73 @@ namespace LimitLess.Area
             }
             return Request.CreateResponse(HttpStatusCode.Created);
         }
+        public Excel.Range openExcelFile(string filePath) {
+            Excel.Application application = new Excel.Application();
+            Excel.Workbook workbook = application.Workbooks.Open(filePath);
+            Excel.Worksheet worksheet = workbook.ActiveSheet;
+            Excel.Range range = worksheet.UsedRange;
+            return range;
+        }
+        public bool checkExcelFormat(Excel.Range standard_range, Excel.Range upload_range)
+        {
+            var headerLine = 1;
+            var numFixedHead = 11;
+            for (int headItem = 1; headItem < numFixedHead; headItem++) {
+                if (((Excel.Range)standard_range.Cells[headerLine, headItem]).Text != ((Excel.Range)upload_range.Cells[headerLine, headItem]).Text) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        public List<SpreadsheetModel> SaveDataToList(Excel.Range range)
+        {
+            var spreadsheetList = new List<SpreadsheetModel>();
+            for (int row = 2; row <= range.Rows.Count; row++)
+            {
+                //if question content is not none, add this row of data
+                if (((Excel.Range)range.Cells[row, 6]).Text != "")
+                {
+                    var queModel = new QuestionModel();
+                    var ansList = new List<AnswerModel>();
+                    var spr = new SpreadsheetModel
+                    {
+                        questionModel = queModel,
+                        answerList = ansList
+                    };
+                    //add question
+                    spr.questionModel.SubObjectiveID = Int32.Parse(extractID(((Excel.Range)range.Cells[row, 5]).Text), CultureInfo.InvariantCulture);
+                    spr.questionModel.QuestionContent = ((Excel.Range)range.Cells[row, 6]).Text;
+                    spr.questionModel.QuestionTypeId = extractID(((Excel.Range)range.Cells[row, 5]).Text);
+                    spr.questionModel.Difficulty = Int32.Parse(extractID(((Excel.Range)range.Cells[row, 8]).Text), CultureInfo.InvariantCulture);
+                    spr.questionModel.QuestionCode = ((Excel.Range)range.Cells[row, 9]).Text;
+                    spr.questionModel.IsActive = ((Excel.Range)range.Cells[row, 10]).Text;
 
+                    //if there is answer content, add the answer
+                    int ans_index = 12;
+                    while (((Excel.Range)range.Cells[row, ans_index]).Text != "")
+                    {
+                        var ans = new AnswerModel();
+                        ans.AnswerContent = ((Excel.Range)range.Cells[row, ans_index]).Text;
+                        ans.Explanation = ((Excel.Range)range.Cells[row, ans_index + 1]).Text;
+                        ans.AnswerCode = ((Excel.Range)range.Cells[row, ans_index + 2]).Text;
+                        ans.IsActive = ((Excel.Range)range.Cells[row, ans_index + 3]).Text;
+                        ans.IsCorrect = ((Excel.Range)range.Cells[row, ans_index + 4]).Text;
+                        spr.answerList.Add(ans);
+                        ans_index += 5;
+                    }
+
+                    spreadsheetList.Add(spr);
+                }
+
+            }
+            return spreadsheetList;
+        }
+        public string extractID(string str)
+        {
+            var regex = new Regex(@"([1-9]+)");
+            var results = regex.Matches(str);
+            var id = results[0].Groups[1].Value;
+            return id;
+        }
     }
 }
--- Limitless/LimitLess/Modules/Spreadsheet/Controller/SpreadsheetController.js
+++ Limitless/LimitLess/Modules/Spreadsheet/Controller/SpreadsheetController.js
@@ -2,12 +2,10 @@
 app.controller('SpreadsheetController', ['$scope', 'Upload', '$timeout', function ($scope, Upload, $timeout) {
 
     $scope.uploadFiles = function (file, errFiles) {
-        console.log("a");
         //upload spreadsheet
         $scope.f = file;
         $scope.errFile = errFiles && errFiles[0];
         if (file) {
-            console.log(file);
             file.upload = Upload.upload({
                 method: 'POST',
                 url: 'http://localhost:60142/api/Spreadsheet/SaveSpreadsheet',
@@ -15,25 +13,27 @@ app.controller('SpreadsheetController', ['$scope', 'Upload', '$timeout', functio
                 //headers: { 'Content-Type': 'application/vnd.ms-excel' }
             });
 
-            file.upload.then(function (response) {
-                $timeout(function () {
-                    file.result = response.data;
+            file.upload.then(
+                function (response) {
+                    console.log(response)
+                    toastr["success"]("Saved Successfully.", 'Upload spreadsheet');
+                    $timeout(function () {
+                        file.result = response.data;
+                    });
+
+                }, function (err) {
+                    toastr["error"]('Failed to upload spreadsheet', 'Upload spreadsheet');
+                    if (response.status > 0)
+                        $scope.errorMsg = response.status + ': ' + response.data;
+                }, function (evt) {
+                    file.progress = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
                 });
-            }, function (response) {
-                if (response.status > 0)
-                    $scope.errorMsg = response.status + ': ' + response.data;
-            }, function (evt) {
-                file.progress = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
-            });
         }
     }
     $scope.uploadImages = function (files, errFiles) {
-        console.log("1:"+files);
         $scope.files = files;
         $scope.errFiles = errFiles;
-        console.log("2:" + files);
         angular.forEach(files, function (file) {
-            console.log("3:" + file);
             file.upload = Upload.upload({
                 method: 'POST',
                 url: 'http://localhost:60142/api/Spreadsheet/SaveImages',
