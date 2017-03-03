ec9e9a30cb26c1547f34b1c17ee3209c45a6a8ae
DESKTOP-DSRH9A9\ADSIXPS15 <zcao@goadsi.com>
2017-03-03 17:04:15 -0500

    generate downloadable spreadsheet upload result log(txt)

--- Limitless/LimitLess/Modules/Spreadsheet/Controller/SpreadsheetController.js
+++ Limitless/LimitLess/Modules/Spreadsheet/Controller/SpreadsheetController.js
@@ -10,7 +10,6 @@ app.controller('SpreadsheetController', ['$scope', 'Upload', '$timeout', functio
                 method: 'POST',
                 url: 'http://localhost:60142/api/Spreadsheet/SaveSpreadsheet',
                 data: { file: file },
-                //headers: { 'Content-Type': 'application/vnd.ms-excel' }
             });
 
             file.upload.then(
@@ -20,6 +19,7 @@ app.controller('SpreadsheetController', ['$scope', 'Upload', '$timeout', functio
                         toastr["success"](response["data"][1], "Saved Successfully.");
                         if (response["data"][0] == "2") {                               // part inserted
                             //automatically download txt, the txt file include the response[data] except for response[data][0]
+                            SaveAsFile(response["data"], "result_log.txt", "text/plain;charset=utf-8");
                         }
                     } else {
                         toastr["error"](response["data"][1], "Saved Failure.");
@@ -31,10 +31,21 @@ app.controller('SpreadsheetController', ['$scope', 'Upload', '$timeout', functio
                 }, function (err) {
                     if (response.status > 0)
                         $scope.errorMsg = response.status + ': ' + response.data;
+                    
                 }, function (evt) {
                     file.progress = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
                 });
         }
+
+        function SaveAsFile(t, f, m) {
+            try {
+                var b = new Blob([t], { type: m });
+                saveAs(b, f);
+            } catch (e) {
+                window.open("data:" + m + "," + encodeURIComponent(t), '_blank', '');       
+            }
+        }
     }
+    
 
 }]);
