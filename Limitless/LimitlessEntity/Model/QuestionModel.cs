048c50a017b8f0eac32693be8161e9832c6f3bf6
DESKTOP-DSRH9A9\ADSIXPS15 <zcao@goadsi.com>
2017-03-22 10:21:32 -0400

    add subobjectivecode attribute to question

--- Limitless/LimitlessEntity/Model/QuestionModel.cs
+++ Limitless/LimitlessEntity/Model/QuestionModel.cs
@@ -5,6 +5,7 @@
         public string QuestionID { get; set; }
         public int SubObjectiveID { get; set; }
         public string QuestionCode { get; set; }
+        public string SubObjectiveCode { get; set; }
         public string QuestionContent { get; set; }
         public int Difficulty { get; set; }
         public string IsActive { get; set; }
